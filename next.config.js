d260bd180027d34d226287c0c36198074830ed44
Virginia Kuhlen <virginia.kuhlen@gmx.de>
2025-09-18 19:08:40 +0200

    Convert next.config.ts to next.config.js to fix Netlify build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -16,4 +15,4 @@ const nextConfig: NextConfig = {
   skipMiddlewareUrlNormalize: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
